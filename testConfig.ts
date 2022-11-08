11672d99e5b53194c38cb71ed96f50e5c8259acc
vivek-consensys <106310394+vivek-consensys@users.noreply.github.com>
2022-11-08 17:03:08 +0000

    Delete testConfig.ts

--- testConfig.ts
+++
@@ -1,8 +0,0 @@
-export const testConfig = {
-    url: 'https://www.saucedemo.com/',
-    successfulUsername: 'standard_user',
-    lockedOutUsername: 'locked_out_user',
-    problemUsername: 'problem_user',
-    performanceGlitchUsername: 'performance_glitch_user',
-    password: `secret_sauce`
-}
